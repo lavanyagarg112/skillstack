bdecd796a1d3dbf7b7aff58de32dacaead236a70
lavanyagarg112 <lavanyagarg@u.nus.edu>
2025-05-24 22:17:20 +0800

    Add page for organisation courses

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -1,8 +1,34 @@
-export default function CoursesPage() {
+import { getAuthUser } from "@/lib/auth";
+
+export default async function SettingsPage() {
+  const user = await getAuthUser();
+  if (!user) {
+    return null;
+  }
+  const organisation = user.organisation;
+  const role = organisation.role;
+  const isAdmin = role === "admin";
+  const isMember = role === "employee";
+
+  if (isAdmin) {
+    return (
+      <div>
+        <h1>Organisation Courses</h1>
+      </div>
+    );
+  }
+
+  if (isMember) {
+    return (
+      <div>
+        <h1>Member Settings</h1>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <h1>Courses</h1>
-      <p>List of courses will be displayed here.</p>
+      <h1>Page should not reach here</h1>
     </div>
   );
 }
